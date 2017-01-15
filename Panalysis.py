694e366f181cc97e670b4fbb29ab1b3e3377c805
Abhay Chatrvedi <abhay447@gmail.com>
2017-01-15 16:38:59 +0530

    added index reseting after sort

--- Panalysis.py
+++ Panalysis.py
@@ -8,6 +8,7 @@ handle.close()
 df = pd.DataFrame(list(myDict.iteritems()),columns=['word','times'])
 df.loc[df['word']=='#','times'] = 0
 df = df.sort(['times'],ascending=[False])
+df.reset_index(drop=True)
 sdf = df[0:50]
 ax = sdf.plot(kind='bar', title ="Topics", x='word', y='times')
 ax.set_xlabel("Topic", fontsize=12)
